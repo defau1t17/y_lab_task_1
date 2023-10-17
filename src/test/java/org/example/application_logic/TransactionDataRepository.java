0f17831a50aa975852764f4042c8332f528066dd
defau1t17 <mihail.stepushin@gmail.com>
2023-10-17 21:29:58 +0700

    Небольшой код ревью. Добавлены тесты для всех основных классов транзаций и клиента

--- src/test/java/org/example/application_logic/TransactionDataRepository.java
+++ src/test/java/org/example/application_logic/TransactionDataRepository.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class OperationDataTest {
+public class TransactionDataRepository {
     private Client client;
 
 
